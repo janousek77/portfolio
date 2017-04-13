6cc7a9519d061fe2fcba66a06c14f5ce10efdf57
janousek77 <janousek77@gmail.com>
2017-04-12 20:20:29 -0700

    fixed constructor function

--- app.js
+++ app.js
@@ -1,61 +1,54 @@
 'use strict';
 
-var projectArray = [];
+var articles = [];
 
 var projectData = [
+  {
+    title:       'Project Animal Rescue',
+    image:       'images/par.png',
+    link:        'https://janousek77.github.io/PAR/',
+    deployedOn:  '2017-03-05'
+  },
   {
     title:       'Bus Mall',
     image:       'images/bus.png',
     link:        'https://janousek77.github.io/bus-mall/',
-    deployedOn:  '2015-11-05'
+    deployedOn:  '2017-03-05'
   },
   {
     title:       'Cookie Store',
     image:       'images/cookie.png',
     link:        'https://janousek77.github.io/cookie-stand/',
-    deployedOn:  '2015-11-05'
-  },
-  {
-    title:       'Project Animal Rescue',
-    image:       'images/par.png',
-    link:        'https://janousek77.github.io/PAR/',
-    deployedOn:  '2015-11-05'
-  },
+    deployedOn:  '2017-03-05'
+  }
 ];
 
-function Project (obj) {
+function Article (obj) {
   this.title = obj.title;
   this.link = obj.link;
   this.image = obj.image;
   this.deployedOn = obj.deployedOn
 }
 
-Project.prototype.toHtml = function() {
-  var $newprojectData = $('').clone();
-  $newprojectData.removeClass('');
-
-
-  if (!this.publishedOn) $newprojectData.addClass('');
-  $newprojectData.find('address a').html(this.author);
-  $newprojectData.find('address a').attr('href', this.authorUrl);
-  $newprojectData.find('h1').html(this.title);
-  $newprojectData.data('category', this.category);
-  $newprojectData.find('.article-body').html(this.body);
-  $newprojectData.find('time datetime').html(this.publishedOn);
-
-  $newprojectData.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-  $newprojectData.append('<hr>');
-  return $newprojectData;
+Article.prototype.toHtml = function() {
+  var $newArticle = $('article.template').clone();
+  $newArticle.removeClass('template');
+  if (!this.deployedOn) $newArticle.addClass('draft');
+  $newArticle.find('h2').text(this.title);
+  $newArticle.find('img').attr('src', this.image);
+  $newArticle.find('a').attr('href', this.link);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.deployedOn))/60/60/24/1000) + ' days ago');
+  return $newArticle;
 };
 
 projectData.sort(function(a,b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  return (new Date(b.deployedOn)) - (new Date(a.deployedOn));
 });
 
 projectData.forEach(function(articleObject) {
-  projectArray.push(new Project(articleObject));
+  articles.push(new Article(articleObject));
 });
 
-projectArray.forEach(function(article) {
+articles.forEach(function(article) {
   $('#articles').append(article.toHtml());
 });
