ef261c458462c9057c9426fc8bfb1784b3236726
janousek77 <janousek77@gmail.com>
2017-04-21 08:48:15 -0700

    took out extraneous code

--- server.js
+++ server.js
@@ -2,17 +2,13 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.get('/', function(request, response){
   response.sendFile('public/index.html', {root: '.'});
 });
 
 app.use(express.static('./public'));
-
-// app.get('/*', function(request, response){
-//   response.status(404).sendFile('public/404.html', {root: '.'});
-// });
 
 app.listen(port, function(){
   console.log(`our server is running on port: ${port}`);
